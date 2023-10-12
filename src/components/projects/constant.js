af6183b7cc4f529b3d43b1fa6e1d62c55dc2d5c0
shreeyansh-29 <shreeyansh.rawat@gmail.com>
2023-10-12 17:20:20 +0530

    added new project

--- src/components/projects/constant.js
+++ src/components/projects/constant.js
@@ -5,7 +5,7 @@ export const data = [
     description:
       "Made an URL Shortener Chrome Extension using ReactJs for frontend and Node, Express for backend.",
     link: "https://github.com/shreeyansh-29/URL-Shortener/tree/master",
-    img: require("../../images/gifs/URL-Shortner.gif")
+    img: require("../../images/gifs/URL-Shortner.gif"),
   },
   {
     projectName: "Interview Preparation Website",
@@ -13,7 +13,14 @@ export const data = [
     description:
       "Designed a Interview preparation website using ReactJs and BootStrap for frontend. Have selected and compiled 150+ top interview questions for technical interview preparation on website.",
     link: "https://github.com/shreeyansh-29/Interview-Preparation",
-    img: require("../../images/gifs/CodePedia.gif")
+    img: require("../../images/gifs/CodePedia.gif"),
+  },
+  {
+    projectName: "Personal Portfolio",
+    techStack: "ReactJS - FramerMotion - EmailJS",
+    link: "https://github.com/shreeyansh-29/portfolio",
+    img: require("../../images/gifs/Portfolio.gif"),
+    description: "Created my personal portfolio to enchance my frontend skills, show casing my experience and techs I'm known of.",
   },
   {
     projectName: "Chat Application",
